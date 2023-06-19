6336c9d4321fa86b40badf182780b3c2b91d7db6
OlgaDobrydina <125290861+OlgaDobrydina@users.noreply.github.com>
2023-06-19 22:20:00 +0500

    Delete HW2.py

--- HW2.py
+++
@@ -1,9 +0,0 @@
-#Найдите сумму цифр трехзначного числа.#
-n = int(input("Введите трехзначное число "))
-summa = 0
-summa = summa + n % 10
-n = n //10
-summa = summa + n % 10
-n = n //10
-summa = summa + n % 10
-print(summa)
